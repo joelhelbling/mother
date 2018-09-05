2c5b0d91de1735dc2c1b32ff76d29a4a266bf03c
Joel Helbling <joel@joelhelbling.com>
2018-09-05 10:15:48 -0400

    Initial implementation

--- lib/mother.rb
+++ lib/mother.rb
@@ -1,5 +1,40 @@
 require "mother/version"
 
-module Mother
-  # Your code goes here...
+class Mother
+  attr_accessor :data
+
+  def initialize(argument)
+    @data =
+    case argument
+    when Hash
+      argument
+    when String
+      case
+      when argument.match(/\.ya?ml$/)
+        YAML.load(File.read(argument))
+      when argument.match(/\.json$/)
+        JSON.parse(File.read(argument))
+      else
+        raise ArgumentError.new('Must be a hash, or YAML/JSON filename')
+      end
+    end
+  end
+
+  def [](key)
+    child = ___fetch key
+    case child
+    when Hash
+      self.class.new child
+    else
+      child
+    end
+  end
+
+  def method_missing(method, *args, &block)
+    self[method]
+  end
+
+  def ___fetch(key)
+    @data[key.to_sym] or @data[key.to_s]
+  end
 end
--- spec/mother_spec.rb
+++ spec/mother_spec.rb
@@ -1,9 +1,61 @@
 RSpec.describe Mother do
-  it "has a version number" do
-    expect(Mother::VERSION).not_to be nil
+  describe '#initialize' do
+    Given(:hash) do
+      { foo: 'phew', bar: 'baer' }
+    end
+
+    context 'when argument is a hash' do
+      Given(:argument) { hash }
+      When(:subject) { described_class.new argument }
+      Then { subject.foo == hash[:foo] }
+    end
+
+    context 'when argument is a filename', :use_fakefs do
+      When(:subject) { described_class.new argument }
+
+      context 'for a .yaml file' do
+        Given(:argument) { 'my.yaml' }
+        Given { write_yaml argument, hash }
+
+        Then { subject.foo == hash[:foo] }
+      end
+
+      context 'for a .yml file' do
+        Given(:argument) { 'my.yml' }
+        Given { write_yaml argument, hash }
+
+        Then { subject.foo == hash[:foo] }
+      end
+
+      context 'for a .json file'  do
+        Given(:argument) { 'my.json' }
+        Given { write_json argument, hash }
+
+        Then { subject.foo == hash[:foo] }
+      end
+    end
+
+    context 'when string is just a string' do
+      Given(:argument) { 'foo, um, bar' }
+      Then do
+        expect{ described_class.new argument }.to raise_error(ArgumentError)
+      end
+    end
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe 'children' do
+    When(:subject) { described_class.new argument }
+
+    context 'who are hashes' do
+      Given(:argument) { { foo: { bar: 'bing' } } }
+
+      Then { expect(subject.foo).to be_a(described_class) }
+    end
+
+    context 'who happen to be mothers' do
+      Given(:argument) { { foo: described_class.new({bar: 'bing'}) } }
+
+      Then { expect(subject.foo).to be_a(described_class) }
+    end
   end
 end
